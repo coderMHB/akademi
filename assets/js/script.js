10cc2a49c2fbbb64ca0ad129fe2719181c1124c6
mhbcoder <mhbcoder@gmail.com>
2024-10-25 12:57:04 +0330

    web

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
 const playersData = {
   salon1: ["علی", "رضا", "حمید", "فرزاد", "کامران"],
-  salon2: ["محمد حسین کریمی","احمدرضا خدری","امیرحسین رسول زاده","محمد گودرزی","محمدحسن اشرفی","محمد هادی قربانی","محمد باقری","(آبی)حسین شم آبادی","حسین شم آبادی(سفید)","طاها شمس نژاد","امیرطاها خیراللهی","ابوالفضل باباجانیان","احمدرضا نحوی نیا","محمد شکوهی","محمد حسین عوادی","علی اکبر امیدی","ابوالفضل عظیمی","علیرضا مظفری","محمدکاظم قلی زاده","محمدمحسن بیگی","صالح میرصالحی","محمدحسین دهقان","علیرضا رحیمی",],
+  salon2: [
+    "محمد حسین کریمی",
+    "احمدرضا خدری",
+    "امیرحسین رسول زاده",
+    "محمد گودرزی",
+    "محمدحسن اشرفی",
+    "محمد هادی قربانی",
+    "محمد باقری",
+    "(آبی)حسین شم آبادی",
+    "حسین شم آبادی(سفید)",
+    "طاها شمس نژاد",
+    "امیرطاها خیراللهی",
+    "ابوالفضل باباجانیان",
+    "احمدرضا نحوی نیا",
+    "محمد شکوهی",
+    "محمد حسین عوادی",
+    "علی اکبر امیدی",
+    "ابوالفضل عظیمی",
+    "علیرضا مظفری",
+    "محمدکاظم قلی زاده",
+    "محمدمحسن بیگی",
+    "صالح میرصالحی",
+    "محمدحسین دهقان",
+    "علیرضا رحیمی",
+  ],
   salon3: ["سارا", "مینا", "فاطمه"],
-  salon4: ["علیرضا", "محمد", "یوسف"]
+  salon4: ["علیرضا", "محمد", "یوسف"],
 };
 
 const termData = {
   term1: { name: "ترم آبان", price: 450 },
   term2: { name: "ترم آذر", price: 450 },
   term3: { name: "ترم دی", price: 450 },
-  term4: { name: "ترم بهمن", price: 450 }
+  term4: { name: "ترم بهمن", price: 450 },
 };
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -18,7 +42,7 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 function showPlayers(salon) {
   const playerTitle = document.getElementById("player-title");
   playerTitle.innerText = `اسامی بازیکنان این شعبه `;
-  
+
   const playerList = document.getElementById("player-list");
   playerList.innerHTML = "";
 
@@ -82,7 +106,9 @@ function displayCart() {
   });
 
   const total = document.createElement("li");
-  total.innerText = cart.length ? `مجموع: ${totalPrice},000 تومان` : "سبد خرید خالی است";
+  total.innerText = cart.length
+    ? `مجموع: ${totalPrice},000 تومان`
+    : "سبد خرید خالی است";
   total.style.fontWeight = "bold";
   cartItemsList.appendChild(total);
 }
@@ -115,8 +141,29 @@ function displayCartMessage(message) {
   messageDiv.innerText = message;
   messageDiv.className = "cart-message";
   document.body.appendChild(messageDiv);
-  // setTimeout(() => {
-  //   messageDiv.style.opacity = "0";
-  //   setTimeout(() => messageDiv.remove(), 500);
-  // }, 3000);
+  setTimeout(() => {
+    messageDiv.style.opacity = "0";
+    setTimeout(() => messageDiv.remove(), 500);
+  }, 3000);
 }
+
+function downloadInvoice() {
+  const invoiceElement = document.getElementById("cart");
+  html2canvas(invoiceElement).then((canvas) => {
+    const image = canvas.toDataURL("image/jpg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "invoice.png"; // نام فایل فاکتور
+    link.click();
+  });
+}
+
+document
+  .getElementById("download-invoice")
+  .addEventListener("click", function () {
+    // نمایش پاپ‌آپ
+    var paymentModal = new bootstrap.Modal(
+      document.getElementById("paymentModal")
+    );
+    paymentModal.show();
+  });
